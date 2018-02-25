b9f1fdf0f22cf1029ee26109cb7c7f519aabbb82
Pedro Ruiz Hidalgo <correo@pedroruizhidalgo.es>
2018-02-25 10:45:14 +0100

    all templates are done

--- application/third_party/ion_auth/views/auth/change_password.php
+++ application/third_party/ion_auth/views/auth/change_password.php
@@ -1,25 +1,74 @@
-<h1><?php echo lang('change_password_heading');?></h1>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div id="infoMessage"><?php echo $message;?></div>
+    <title>Gentelella Alela! | <?php echo lang('change_password_heading');?></title>
 
-<?php echo form_open("auth/change_password");?>
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/animate.css/animate.min.css" rel="stylesheet">
 
-      <p>
-            <?php echo lang('change_password_old_password_label', 'old_password');?> <br />
-            <?php echo form_input($old_password);?>
-      </p>
+    <!-- Custom Theme Style -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/build/css/custom.min.css" rel="stylesheet">
+  </head>
 
-      <p>
-            <label for="new_password"><?php echo sprintf(lang('change_password_new_password_label'), $min_password_length);?></label> <br />
-            <?php echo form_input($new_password);?>
-      </p>
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
 
-      <p>
-            <?php echo lang('change_password_new_password_confirm_label', 'new_password_confirm');?> <br />
-            <?php echo form_input($new_password_confirm);?>
-      </p>
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+              <div id="infoMessage"><?php echo $message;?></div>
+            <?php echo form_open("auth/change_password");?>
+              <h1><?php echo lang('change_password_heading');?></h1>
+              <div>
+                  <?php echo lang('change_password_old_password_label', 'old_password');?>
+                  <input name="old_password" type="text" class="form-control" placeholder="Old Password" required="required" />
+              </div>
+              <div>
+                  <?php echo sprintf(lang('change_password_new_password_label'), $min_password_length);?>
+                  <input name="new_password" type="password" class="form-control" placeholder="Password" required="required" />
+              </div>
+              <div>
+                  <?php echo lang('change_password_new_password_confirm_label', 'new_password_confirm');?>
+                  <input name="new_password_confirm" type="password" class="form-control" placeholder="Password confirm" required="required" />
+              </div>
+              <div>
+                <?php echo form_submit('submit', lang('change_password_submit_btn'),array('class'=>"btn btn-default submit"));?>
 
-      <?php echo form_input($user_id);?>
-      <p><?php echo form_submit('submit', lang('change_password_submit_btn'));?></p>
 
-<?php echo form_close();?>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                  <p>© 2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+
+
+      </div>
+    </div>
+  </body>
+</html>
--- application/third_party/ion_auth/views/auth/deactivate_user.php
+++ application/third_party/ion_auth/views/auth/deactivate_user.php
@@ -1,18 +1,131 @@
-<h1><?php echo lang('deactivate_heading');?></h1>
-<p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<?php echo form_open("auth/deactivate/".$user->id);?>
+    <title>Gentelella Alela! | <?php echo lang('deactivate_heading');?></title>
 
-  <p>
-  	<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
-    <input type="radio" name="confirm" value="yes" checked="checked" />
-    <?php echo lang('deactivate_confirm_n_label', 'confirm');?>
-    <input type="radio" name="confirm" value="no" />
-  </p>
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/nprogress/nprogress.css" rel="stylesheet">
 
-  <?php echo form_hidden($csrf); ?>
-  <?php echo form_hidden(array('id'=>$user->id)); ?>
+    <!-- Custom Theme Style -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/build/css/custom.min.css" rel="stylesheet">
+  </head>
 
-  <p><?php echo form_submit('submit', lang('deactivate_submit_btn'));?></p>
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+            </div>
 
-<?php echo form_close();?>
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>John Doe</h2>
+              </div>
+              <div class="clearfix"></div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <?php $this->load->view('inc/sidebar_menu');?>
+          </div>
+        </div>
+
+        <?php $this->load->view('inc/top_menu');?>
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3><?php echo lang('deactivate_heading');?></h3>
+              </div>
+
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                      <?php echo form_open("auth/deactivate/".$user->id);?>
+
+                        <p>
+                        	<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
+                          <input type="radio" name="confirm" value="yes" checked="checked" />
+                          <?php echo lang('deactivate_confirm_n_label', 'confirm');?>
+                          <input type="radio" name="confirm" value="no" />
+                        </p>
+
+                        <?php echo form_hidden($csrf); ?>
+                        <?php echo form_hidden(array('id'=>$user->id)); ?>
+
+                        <p><?php echo form_submit('submit', lang('deactivate_submit_btn'),array('class'=>'btn btn-primary'));?></p>
+
+                      <?php echo form_close();?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+        <?php $this->load->view('inc/footer');?>
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url('assets/gentelella_master')?>/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url('assets/gentelella_master')?>/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?php echo base_url('assets/gentelella_master')?>/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="<?php echo base_url('assets/gentelella_master')?>/vendors/nprogress/nprogress.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="<?php echo base_url('assets/gentelella_master')?>/build/js/custom.min.js"></script>
+  </body>
+</html>
