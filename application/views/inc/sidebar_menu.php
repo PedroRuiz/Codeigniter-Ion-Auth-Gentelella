4f56e56271d5ccb054fd9099a06412bd930e4f9c
Pedro Ruiz Hidalgo <correo@pedroruizhidalgo.es>
2018-02-25 12:52:38 +0100

    side_menu fixed

--- application/views/inc/sidebar_menu.php
+++ application/views/inc/sidebar_menu.php
@@ -1,3 +1,25 @@
+<div class="col-md-3 left_col">
+  <div class="left_col scroll-view">
+    <div class="navbar nav_title" style="border: 0;">
+      <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <!-- menu profile quick info -->
+    <div class="profile clearfix">
+      <div class="profile_pic">
+        <!--img src="images/img.jpg" alt="..." class="img-circle profile_img"-->
+      </div>
+      <div class="profile_info">
+        <span>Welcome,</span>
+        <h2><?php echo sprintf('%s %s',$this->ion_auth->user()->row()->first_name,$this->ion_auth->user()->row()->last_name);?></h2>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+    <!-- /menu profile quick info -->
+
+    <br />
 <!-- sidebar menu -->
 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
   <div class="menu_section">
