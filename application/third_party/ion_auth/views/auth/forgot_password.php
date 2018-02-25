b99ee9d4c383440881810b3473dfc474505f90b3
Pedro Ruiz Hidalgo <correo@pedroruizhidalgo.es>
2018-02-25 10:41:41 +0100

    all templates are done

--- application/third_party/ion_auth/views/auth/forgot_password.php
+++ application/third_party/ion_auth/views/auth/forgot_password.php
@@ -1,15 +1,61 @@
-<h1><?php echo lang('forgot_password_heading');?></h1>
-<p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label);?></p>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div id="infoMessage"><?php echo $message;?></div>
+    <title>Gentelella Alela! | <?php echo lang('forgot_password_heading');?></title>
 
-<?php echo form_open("auth/forgot_password");?>
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/vendors/animate.css/animate.min.css" rel="stylesheet">
 
-      <p>
-      	<label for="identity"><?php echo (($type=='email') ? sprintf(lang('forgot_password_email_label'), $identity_label) : sprintf(lang('forgot_password_identity_label'), $identity_label));?></label> <br />
-      	<?php echo form_input($identity);?>
-      </p>
+    <!-- Custom Theme Style -->
+    <link href="<?php echo base_url('assets/gentelella_master')?>/build/css/custom.min.css" rel="stylesheet">
+  </head>
 
-      <p><?php echo form_submit('submit', lang('forgot_password_submit_btn'));?></p>
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
 
-<?php echo form_close();?>
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            <?php echo form_open("auth/forgot_password");?>
+              <h1><?php echo lang('forgot_password_heading');?></h1>
+              <p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label);?></p>
+              <div>
+                  <?php echo lang('login_identity_label', 'identity');?>
+                <input name="identity" type="text" class="form-control" placeholder="<?php echo $identity_label;?>" required="required" />
+              </div>
+              <?php echo form_submit('submit', lang('forgot_password_submit_btn'));?>
+              <div class="clearfix"></div>
+
+              <div class="separator">
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                  <p>© 2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+
+
+      </div>
+    </div>
+  </body>
+</html>
